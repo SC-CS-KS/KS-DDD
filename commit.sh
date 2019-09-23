ef9acc067125effaa9e8cb07755457be40f239e1
sunnychan <sunnnychan@gmail.com>
2019-09-23 14:03:25 +0800

    sunnychan

--- commit.sh
+++ commit.sh
@@ -4,6 +4,7 @@
 github() {
    git config --global user.name sunnychan
    git config --global user.email sunnnychan@gmail.com
+   git config core.ignorecase false
    git $@
 }
 github add . && github commit -m "sunnychan" && github push
